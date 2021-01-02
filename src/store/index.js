424fcfa008265ec6346d9e12e22c2556009c9e03
Arda Altinay <ardaltinay48@gmail.com>
2021-01-02 19:12:01 +0300

    added vuex persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -10,5 +11,6 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
-  }
+  },
+  plugins: [createPersistedState()]
 })
